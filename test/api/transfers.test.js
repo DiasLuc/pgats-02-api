7950c825edd58b7edd563a79f04591f9c4ddba7c
Lucas Dias <DiasLucP@gmail.com>
2025-10-10 16:19:31 -0400

    Added GET /transfers tests

--- test/api/transfers.test.js
+++ test/api/transfers.test.js
@@ -14,7 +14,7 @@ describe('Transfers', () => {
         token = (await loginUser("julio", "123456")).body.token;
     });
     describe('POST /transfers', () => {
-        it.only('Should return status code 201, and transfer should be completed successfully', async () => {
+        it('Should return status code 201, and transfer should be completed successfully', async () => {
             const bodyTransfers = { ...postTransfers };
             
             bodyTransfers.from = "julio";
@@ -34,21 +34,81 @@ describe('Transfers', () => {
         });
 
         it('Should return error with status code 400, stating there was a validation or business rule error', async () => {
+            const bodyTransfers = { ...postTransfers };
+            
+            bodyTransfers.from = "julio";
+            bodyTransfers.to = "julio";
+            bodyTransfers.value = 6000;
 
+            const response = await request(process.env.BASE_URL_REST)
+                .post('/transfers')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send(bodyTransfers);
+            
+            expect(response.statusCode).to.equal(400);
+            expect(response.body.error).to.equal("Transferência acima de R$ 5.000,00 só para favorecidos");
         });
 
-        it('Should return error with status code 401, stating token wasn\'t provided or is invalid', async () => {
+        it('Should return error with status code 401, stating token is invalid', async () => {
+            const bodyTransfers = { ...postTransfers };
+            
+            bodyTransfers.from = "julio";
+            bodyTransfers.to = "priscila";
+            bodyTransfers.value = 22;
 
+            const response = await request(process.env.BASE_URL_REST)
+                .post('/transfers')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer INVALID TOKEN`)
+                .send(bodyTransfers);
+            
+            expect(response.statusCode).to.equal(401);
+            expect(response.body.message).to.equal("Token inválido.");
+        });
+
+        it('Should return error with status code 401, stating token wasn\'t provided', async () => {
+            const bodyTransfers = { ...postTransfers };
+            
+            bodyTransfers.from = "julio";
+            bodyTransfers.to = "priscila";
+            bodyTransfers.value = 22;
+
+            const response = await request(process.env.BASE_URL_REST)
+                .post('/transfers')
+                .set('Content-Type', 'application/json')
+                // No token sent:
+                // .set('Authorization', `Bearer ${token}`)
+                .send(bodyTransfers);
+            
+            expect(response.statusCode).to.equal(401);
+            expect(response.body.message).to.equal("Token não fornecido.");
         });
     });
 
     describe('GET /transfers', () => {
         it('Should return status code 200, and return the list of transfers', async () => {
-
+            const response = await request(process.env.BASE_URL_REST)
+                .get('/transfers')
+                .set('Authorization', `Bearer ${token}`)
+            
+            expect(response.statusCode).to.equal(200);
+            expect(response.body).to.be.an('array')
         });
 
-        it('Should return error with status code 401, stating token wasn\'t provided or is invalid', async () => {
+        it('Should return error with status code 401, stating token is invalid', async () => {
+            const response = await request(process.env.BASE_URL_REST)
+                .get('/transfers')
+                .set('Authorization', `Bearer INVALID TOKEN`)
+            
+            expect(response.statusCode).to.equal(401);
+        });
 
+        it('Should return error with status code 401, stating token wasn\'t provided', async () => {
+            const response = await request(process.env.BASE_URL_REST)
+                .get('/transfers')
+
+            expect(response.statusCode).to.equal(401);
         });
     });
 
